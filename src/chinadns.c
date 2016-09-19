0f2374d62b60e4bc0ee3e9c482740ce5e9079d50
Emongxx <Emongxx@gmail.com>
2016-09-19 19:10:08 +0800

    check all answers from foreign. because some domain answer mix with chn and foreign records.

--- src/chinadns.c
+++ src/chinadns.c
@@ -813,7 +813,9 @@ static int should_filter_query(ns_msg msg, struct in_addr dns_addr) {
         if (dns_is_foreign) {
           if (bidirectional) {
             // filter DNS result from foreign dns if result is inside chn
-            return 1;
+            if(rrnum == rrmax - 1) {
+              return 1;
+            }
           }
         }
       } else {
